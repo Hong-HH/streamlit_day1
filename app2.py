23de39441ce7d95989fbae39ce1f5b211b6b3c66
Hong-HH <a1guskn7@gmail.com>
2021-12-13 17:25:25 +0900

    스트림릿 문법 추가

---
+++ app2.py
@@ -0,0 +1,18 @@
+#스트림릿 라이브러리를 사용하기 위한 임포트문 작성
+
+# 스트림릿 라이브러리는 이미 설치했다!
+# pip install stramlit
+
+# 위의 라이브러리는 설치치했으므로, 임포트만 하면 된다
+import streamlit as st
+
+# 웹 대시보드 개발 라이브버리인 스트림릿은 main 함수가 있어야한다.
+def main() :
+    st.title('Hello Streamlit. 웹 대시보드')
+    st.title('헬로우')
+
+if __name__=='__main__':
+    main()
+    
+
+# 터미널에서 ctrl+c 하면 streamlit이 꺼진다.
---
+++ app3.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+
+def main() :
+    # 데이터프레임 읽기만하면 출력을 안한다.
+    df = pd.read_csv('data/iris.csv')
+    st.dataframe(df)
+
+    species = df['species'].unique()
+    st.text('아이리스 꽃은' + species + '으로 되어있다.')
+
+    st.write(df.head())
+
+
+if __name__ == '__main__' :
+    main()
